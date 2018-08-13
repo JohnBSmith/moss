bb6c142c2d1e8d745c3170b5e0957c02bf22f1a5
finn <finn@c1>
2018-08-13 09:08:23 +0200

    update

--- src/modules/la.rs
+++ src/modules/la.rs
@@ -31,9 +31,29 @@ enum Data{
 
 struct Array{
     n: usize,
-    base: usize,
+    base: isize,
     s: Box<[ShapeStride]>,
     data: Rc<RefCell<Data>>
+}
+
+impl Array{
+    fn downcast(x: &Object) -> Option<&Array> {
+        if let Object::Interface(ref a) = *x {
+            a.as_any().downcast_ref::<Array>()
+        }else{
+            None
+        }
+    }
+    fn vector_f64(v: Vec<f64>) -> Rc<Array> {
+        let shape = v.len();
+        let data = Rc::new(RefCell::new(Data::F64(
+            v.into_boxed_slice()
+        )));
+        Rc::new(Array{
+            s: Box::new([ShapeStride{shape, stride: 1}]),
+            n: 1, base: 0, data: data
+        })
+    }
 }
 
 impl Interface for Array {
@@ -56,6 +76,116 @@ impl Interface for Array {
         }else{
             unimplemented!();
         }
+    }
+    fn add(&self, b: &Object, env: &mut Env) -> FnResult {
+        if let Some(b) = Array::downcast(b) {
+            match *self.data.borrow() {
+                Data::F64(ref data) => map_binary_f64(self,data,b,&|x,y| x+y),
+                _ => panic!()
+            }
+        }else{
+            panic!()
+        }
+    }
+    fn sub(&self, b: &Object, env: &mut Env) -> FnResult {
+        if let Some(b) = Array::downcast(b) {
+            match *self.data.borrow() {
+                Data::F64(ref data) => map_binary_f64(self,data,b,&|x,y| x-y),
+                _ => panic!()
+            }
+        }else{
+            panic!()
+        }
+    }
+    fn rmpy(&self, a: &Object, env: &mut Env) -> FnResult {
+        let r = match *a {
+            Object::Int(x) => x as f64,
+            Object::Float(x) => x,
+            _ => return env.type_error(
+                "Type error in r*v: r has to be of type Int or Float.")
+        };
+        match *self.data.borrow() {
+            Data::F64(ref data) => map_unary_f64(self,data,&|x| r*x),
+            _ => panic!()
+        }        
+    }
+    fn index(&self, indices: &[Object], env: &mut Env) -> FnResult {
+        if self.n==1 && indices.len()==1 {
+            match indices[0] {
+                Object::Int(i) => {
+                    let i = i as isize;
+                    if i>=0 && (i as usize)<self.s[0].shape {
+                        match *self.data.borrow_mut() {
+                            Data::F64(ref data) => {
+                                let index = (self.base as isize+self.s[0].stride*i) as usize;
+                                return Ok(Object::Float(data[index]));
+                            },
+                            _ => panic!()
+                        }
+                    }else{
+                        return env.index_error("Index error in a[i]: out of bounds.");
+                    }
+                },
+                ref i => {
+                    return env.type_error1("Type error in a[i]: i is not an integer.","i",i);
+                }
+            }
+        }else{
+            panic!()
+        }
+    }
+}
+
+fn map_binary_f64(a: &Array, adata: &[f64], b: &Array,
+    f: &Fn(f64,f64)->f64
+) -> FnResult
+{
+    if a.n==1 {
+        let astride = a.s[0].stride;
+        let ashape = a.s[0].shape;
+        let abase = a.base;
+        let mut v: Vec<f64> = Vec::with_capacity(ashape);
+
+        if b.n != 1 {
+            panic!()
+        }
+        if ashape != b.s[0].shape {
+            panic!()
+        }
+        let bstride = b.s[0].stride;
+        let bbase = b.base;
+        let bdata_borrow = b.data.borrow();
+        let bdata = match *bdata_borrow {
+            Data::F64(ref data) => data, _ => panic!()
+        };
+        for i in 0..ashape {
+            let aindex = (abase+(i as isize)*astride) as usize;
+            let bindex = (bbase+(i as isize)*bstride) as usize;
+            v.push(f(adata[aindex],bdata[bindex]));
+        }
+        return Ok(Object::Interface(Array::vector_f64(v)));
+
+    }else{
+        unimplemented!();
+    }
+}
+
+fn map_unary_f64(a: &Array, data: &[f64], f: &Fn(f64)->f64)
+-> FnResult
+{
+    if a.n==1 {
+        let stride = a.s[0].stride;
+        let shape = a.s[0].shape;
+        let base = a.base;
+        let mut v: Vec<f64> = Vec::with_capacity(shape);
+        for i in 0..shape {
+            let index = (base+(i as isize)*stride) as usize;
+            v.push(f(data[index]));
+        }
+        return Ok(Object::Interface(Array::vector_f64(v)));
+
+    }else{
+        unimplemented!();
     }
 }
 
